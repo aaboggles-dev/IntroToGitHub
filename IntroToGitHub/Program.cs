223f101ffc9a98a925d66d78bda0713a4cc1d4a2
aaboggles.dev@outlook.com <aaboggles.dev@outlook.com>
2023-12-22 22:41:42 -0500

    test

--- IntroToGitHub/Program.cs
+++ IntroToGitHub/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// this is a test
 
 await builder.Build().RunAsync();
